e90423da99d15c59b1cb73b6e8d702a2daec7c2c
Lotte <lottealdinger@gmail.com>
2021-09-09 15:16:11 +0200

    adjust to FF CableMesh datastructure

--- src/compas_fofin/fofin/equilibrium_fd_numpy.py
+++ src/compas_fofin/fofin/equilibrium_fd_numpy.py
@@ -10,18 +10,18 @@ from scipy.sparse.linalg import spsolve
 from compas.numerical import connectivity_matrix
 from compas.numerical import normrow
 
-from compas_fofin.loads import SelfweightCalculator
+# from compas_fofin.loads import SelfweightCalculator
 
 
 __all__ = ['fd_xyz_numpy']
 
 
-def fd_xyz_numpy(mesh):
+def fd_xyz_numpy(mesh, *args, **kwargs):
     """Find the equilibrium shape of a mesh for the given force densities.
 
     Parameters
     ----------
-    mesh : compas_fofin.datastructures.Cablenet
+    mesh : compas_formfinder.datastructures.CableMesh
         The mesh to equilibriate.
 
     Returns
@@ -36,16 +36,16 @@ def fd_xyz_numpy(mesh):
     free = list(set(range(mesh.number_of_vertices())) - set(fixed))
     xyz = array(mesh.vertices_attributes('xyz'), dtype=float64)
     p = array(mesh.vertices_attributes(('px', 'py', 'pz')), dtype=float64)
-    edges = [(k_i[u], k_i[v]) for u, v in mesh.edges_where({'is_edge': True})]
-    q = array([attr['q'] for key, attr in mesh.edges_where({'is_edge': True}, True)], dtype=float64).reshape((-1, 1))
+    edges = [(k_i[u], k_i[v]) for u, v in mesh.edges_where({'_is_edge': True})]
+    q = array([attr['q'] for key, attr in mesh.edges_where({'_is_edge': True}, True)], dtype=float64).reshape((-1, 1))
 
-    density = mesh.attributes['density']
+    # density = mesh.attributes['density']
 
-    calculate_sw = SelfweightCalculator(mesh, density=density)
+    # calculate_sw = SelfweightCalculator(mesh, density=density)
 
-    if density:
-        sw = calculate_sw(xyz)
-        p[:, 2] = -sw[:, 0]
+    # if density:
+    #     sw = calculate_sw(xyz)
+    #     p[:, 2] = -sw[:, 0]
 
     C = connectivity_matrix(edges, 'csr')
     Ci = C[:, free]
@@ -58,11 +58,11 @@ def fd_xyz_numpy(mesh):
 
     xyz[free] = spsolve(A, b)
 
-    if density:
-        sw = calculate_sw(xyz)
-        p[:, 2] = -sw[:, 0]
+    # if density:
+    #     sw = calculate_sw(xyz)
+    #     p[:, 2] = -sw[:, 0]
 
-    l = normrow(C.dot(xyz))
+    l = normrow(C.dot(xyz))  # NOQA: E741
     f = q * l
     r = p - Ct.dot(Q).dot(C).dot(xyz)
 
@@ -71,19 +71,13 @@ def fd_xyz_numpy(mesh):
         attr['x'] = xyz[index, 0]
         attr['y'] = xyz[index, 1]
         attr['z'] = xyz[index, 2]
-        attr['rx'] = r[index, 0]
-        attr['ry'] = r[index, 1]
-        attr['rz'] = r[index, 2]
+        attr['_rx'] = r[index, 0]
+        attr['_ry'] = r[index, 1]
+        attr['_rz'] = r[index, 2]
 
-    for index, (key, attr) in enumerate(mesh.edges_where({'is_edge': True}, True)):
+    for index, (key, attr) in enumerate(mesh.edges_where({'_is_edge': True}, True)):
         attr['q'] = q[index, 0]
         attr['f'] = f[index, 0]
         attr['l'] = l[index, 0]
 
-
-# ==============================================================================
-# Main
-# ==============================================================================
-
-if __name__ == '__main__':
-    pass
+    return mesh
