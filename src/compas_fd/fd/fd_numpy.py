ea8c378c0a49916518ed65151c6662f5a05e8761
brgcode <vanmelet@ethz.ch>
2021-09-14 23:59:43 +0200

    add result object

--- src/compas_fd/fd/fd_numpy.py
+++ src/compas_fd/fd/fd_numpy.py
@@ -1,6 +1,9 @@
-from typing import List, Tuple
+from typing import List, Tuple, NamedTuple
 from typing_extensions import Annotated
-from numpy import array
+
+from collections import namedtuple
+
+from numpy import asarray
 from numpy import float64
 from scipy.sparse import diags
 from scipy.sparse.linalg import spsolve
@@ -11,45 +14,30 @@ from compas.numerical import normrow
 from compas_fd.loads import SelfweightCalculator
 
 
-Point = Annotated[List[float], 3]
-Load = Annotated[List[float], 3]
-Force = Annotated[List[float], 3]
+Vector3 = Annotated[List[float], 3]
+
+Result = NamedTuple('Result', [
+    ('vertices', List[Vector3]),
+    ('forces', List[float]),
+    ('lenghts', List[float]),
+    ('residuals', List[Vector3])
+])
 
 
 def fd_numpy(*,
-             vertices: List[Point],
+             vertices: List[Vector3],
              edges: List[Tuple[int, int]],
-             loads: List[Load],
+             loads: List[Vector3],
              q: List[float],
-             fixed: List[int]) -> Tuple[List[Point], List[float], List[Force]]:
-    """Find the equilibrium shape of a mesh for the given force densities.
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-    None
-        The function updates the input mesh and returns nothing.
-
+             fixed: List[int]) -> Result:
     """
-    k_i = mesh.key_index()
-    fixed = mesh.vertices_where({'is_anchor': True})
-    fixed = [k_i[key] for key in fixed]
-    free = list(set(range(mesh.number_of_vertices())) - set(fixed))
-    xyz = array(mesh.vertices_attributes('xyz'), dtype=float64)
-    p = array(mesh.vertices_attributes(('px', 'py', 'pz')), dtype=float64)
-    edges = [(k_i[u], k_i[v]) for u, v in mesh.edges_where({'is_edge': True})]
-    q = array([attr['q'] for key, attr in mesh.edges_where({'is_edge': True}, True)], dtype=float64).reshape((-1, 1))
-
-    density = mesh.attributes['density']
-
-    calculate_sw = SelfweightCalculator(mesh, density=density)
-
-    if density:
-        sw = calculate_sw(xyz)
-        p[:, 2] = -sw[:, 0]
-
+    Compute the equilibrium coordinates of a system of vertices connected by edges.
+    """
+    v = len(vertices)
+    free = list(set(range(v)) - set(fixed))
+    xyz = asarray(vertices, dtype=float64).reshape((-1, 3))
+    q = asarray(q, dtype=float64).reshape((-1, 1))
+    p = asarray(loads, dtype=float64).reshape((-1, 3))
     C = connectivity_matrix(edges, 'csr')
     Ci = C[:, free]
     Cf = C[:, fixed]
@@ -58,27 +46,8 @@ def fd_numpy(*,
     Q = diags([q.flatten()], [0])
     A = Cit.dot(Q).dot(Ci)
     b = p[free] - Cit.dot(Q).dot(Cf).dot(xyz[fixed])
-
     xyz[free] = spsolve(A, b)
-
-    if density:
-        sw = calculate_sw(xyz)
-        p[:, 2] = -sw[:, 0]
-
-    l = normrow(C.dot(xyz))
-    f = q * l
-    r = p - Ct.dot(Q).dot(C).dot(xyz)
-
-    for key, attr in mesh.vertices(True):
-        index = k_i[key]
-        attr['x'] = xyz[index, 0]
-        attr['y'] = xyz[index, 1]
-        attr['z'] = xyz[index, 2]
-        attr['rx'] = r[index, 0]
-        attr['ry'] = r[index, 1]
-        attr['rz'] = r[index, 2]
-
-    for index, (key, attr) in enumerate(mesh.edges_where({'is_edge': True}, True)):
-        attr['q'] = q[index, 0]
-        attr['f'] = f[index, 0]
-        attr['l'] = l[index, 0]
+    lengths = normrow(C.dot(xyz))  # noqa: E741
+    forces = q * lengths
+    residuals = p - Ct.dot(Q).dot(C).dot(xyz)
+    return Result('result', [xyz, forces, lengths, residuals])
--- src/compas_fd/fd/mesh_fd_numpy.py
+++ src/compas_fd/fd/mesh_fd_numpy.py
@@ -1,10 +1,7 @@
 from numpy import array
 from numpy import float64
-from scipy.sparse import diags
-from scipy.sparse.linalg import spsolve
 
-from compas.numerical import connectivity_matrix
-from compas.numerical import normrow
+from .fd_numpy import fd_numpy
 
 from compas_fd.loads import SelfweightCalculator
 
@@ -26,28 +23,25 @@ def mesh_fd_numpy(mesh):
     k_i = mesh.key_index()
     fixed = mesh.vertices_where({'is_anchor': True})
     fixed = [k_i[key] for key in fixed]
-    free = list(set(range(mesh.number_of_vertices())) - set(fixed))
     xyz = array(mesh.vertices_attributes('xyz'), dtype=float64)
     p = array(mesh.vertices_attributes(('px', 'py', 'pz')), dtype=float64)
     edges = [(k_i[u], k_i[v]) for u, v in mesh.edges_where({'is_edge': True})]
     q = array([attr['q'] for key, attr in mesh.edges_where({'is_edge': True}, True)], dtype=float64).reshape((-1, 1))
-
     density = mesh.attributes['density']
-
     calculate_sw = SelfweightCalculator(mesh, density=density)
+    p[:, 2] -= calculate_sw(xyz)
 
-    # replace by fd_numpy
+    result = fd_numpy(vertices=xyz, edges=edges, loads=p, q=q, fixed=fixed)
 
     for key, attr in mesh.vertices(True):
         index = k_i[key]
-        attr['x'] = xyz[index, 0]
-        attr['y'] = xyz[index, 1]
-        attr['z'] = xyz[index, 2]
-        attr['rx'] = r[index, 0]
-        attr['ry'] = r[index, 1]
-        attr['rz'] = r[index, 2]
+        attr['x'] = result.vertices[index, 0]
+        attr['y'] = result.vertices[index, 1]
+        attr['z'] = result.vertices[index, 2]
+        attr['_rx'] = result.residuals[index, 0]
+        attr['_ry'] = result.residuals[index, 1]
+        attr['_rz'] = result.residuals[index, 2]
 
     for index, (key, attr) in enumerate(mesh.edges_where({'is_edge': True}, True)):
-        attr['q'] = q[index, 0]
-        attr['f'] = f[index, 0]
-        attr['l'] = l[index, 0]
+        attr['_f'] = result.forces[index, 0]
+        attr['_l'] = result.lenghts[index, 0]
