74f6fb7b3da07cbc71669ae472ade6d7113cd7d5
brgcode <vanmelet@ethz.ch>
2021-09-15 09:22:46 +0200

    basic test

---
+++ tests/test_hypar.py
@@ -0,0 +1,15 @@
+import numpy as np
+from compas_fd.fd import fd_numpy
+
+
+def test_hypar():
+    o = [0, 0, 0]
+    a = [0, 0, 1]
+    b = [1, 0, 0]
+    c = [1, 1, 1]
+    d = [0, 1, 0]
+    vertices = [o, a, b, c, d]
+    edges = [(0, 1), (0, 2), (0, 3), (0, 4)]
+    forcedensities = [1, 1, 1, 1]
+    result = fd_numpy(vertices=vertices, fixed=[1, 2, 3, 4], edges=edges, forcedensities=forcedensities)
+    assert np.allclose(result.vertices[0], [0.5, 0.5, 0.5])
