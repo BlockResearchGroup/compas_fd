ade278fe6baa94300df87a0ea5b5c9b3b130b691
Lotte <lottealdinger@gmail.com>
2021-11-02 19:04:21 +0100

    add constraint types 2

--- src/compas_fd/constraints/__init__.py
+++ src/compas_fd/constraints/__init__.py
@@ -3,8 +3,17 @@ from __future__ import absolute_import
 from __future__ import division
 
 from compas.geometry import Line
+from compas.geometry import Plane
+from compas_occ.geometry import OCCNurbsCurve as NurbsCurve
+from compas_occ.geometry import OCCNurbsSurface as NurbsSurface
 
 from .constraint import Constraint
 from .lineconstraint import LineConstraint
+from .planeconstraint import PlaneConstraint
+from .curveconstraint import CurveConstraint
+from .curveconstraint import SurfaceConstraint
 
 Constraint.GEOMETRY_CONSTRAINT[Line] = LineConstraint
+Constraint.GEOMETRY_CONSTRAINT[Plane] = PlaneConstraint
+Constraint.GEOMETRY_CONSTRAINT[NurbsCurve] = CurveConstraint
+Constraint.GEOMETRY_CONSTRAINT[NurbsSurface] = SurfaceConstraint
