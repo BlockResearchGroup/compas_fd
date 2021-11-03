de618ac17f904d33bd4e9efb9cfb4ddb12da7508
Lotte <lottealdinger@gmail.com>
2021-11-03 18:42:03 +0100

    cleanup

--- scripts/test_constraints_surface.py
+++ scripts/test_constraints_surface.py
@@ -1,6 +1,6 @@
 from functools import partial
 import compas
-from compas.geometry import Vector, Point, Line, Plane
+from compas.geometry import Vector, Point, Line
 from compas_fd.datastructures import CableMesh
 from compas_fd.fd import fd_numpy
 
@@ -25,15 +25,11 @@ points = [
     [Point(0, 6, 0), Point(3, 6, 6), Point(6, 6, 6), Point(9, 6, 0)],
     [Point(0, 9, 10), Point(3, 9, 10), Point(6, 9, 10), Point(9, 9, 10)],
 ]
-
 surface = NurbsSurface.from_points(points=points)
-
 constraint = Constraint(surface)
-
 mesh.vertex_attribute(vertex, 'constraint', constraint)
 
 vertex_index = mesh.vertex_index()
-
 vertices = mesh.vertices_attributes('xyz')
 edges = [(vertex_index[u], vertex_index[v]) for u, v in mesh.edges_where({'_is_edge': True})]
 loads = mesh.vertices_attributes(['px', 'py', 'pz'])
@@ -71,12 +67,7 @@ for vertex in fixed:
 
 for constraint in constraints:
     if constraint:
-        # viewer.add(constraint.geometry, linewidth=5, linecolor=(0, 1, 1))
-
-        # viewer.add(Polyline(constraint.geometry.locus()), linewidth=5, linecolor=(0, 1, 1))
-
-        viewer.add(surface.to_mesh(nu=100))
-
+        viewer.add(surface.to_mesh(nu=10))
 
 for vertex in fixed:
     a = Point(* mesh.vertex_attributes(vertex, 'xyz'))
