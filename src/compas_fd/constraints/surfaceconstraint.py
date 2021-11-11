c11628283a113ef26d8a97a778b7e886d8912bff
Sam <Sam_Bouten@hotmail.com>
2021-11-11 18:20:06 +0100

    Fix project parameters name

--- src/compas_fd/constraints/surfaceconstraint.py
+++ src/compas_fd/constraints/surfaceconstraint.py
@@ -46,5 +46,5 @@ class SurfaceConstraint(Constraint):
         self._normal = Vector(* vector_component(self.residual, normal))
 
     def project(self):
-        xyz, self._param = self.geometry.closest_point(self._location, parameter=True)
+        xyz, self._param = self.geometry.closest_point(self._location, return_parameter=True)
         self._location = Point(* xyz)
