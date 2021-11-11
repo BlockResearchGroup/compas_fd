d6954b2c640afbf1c8221d0ca1312de2c294288b
brgcode <vanmelet@ethz.ch>
2021-11-11 15:50:37 +0100

    update test

--- scripts/test_constraintdata.py
+++ scripts/test_constraintdata.py
@@ -1,14 +1,13 @@
-import compas
 from compas.geometry import Line
 from compas.geometry import Plane
 from compas.geometry import NurbsCurve
-from compas.geometry import NurbsSurface
 from compas_fd.datastructures import CableMesh
 from compas_fd.constraints import Constraint
 
 from compas.rpc import Proxy
 
 fofin = Proxy('compas_fd.fd')
+fofin.restart_server()
 
 constraints = [
     Constraint(Line([0, 0, 0], [1, 0, 0])),
@@ -23,16 +22,11 @@ corners = list(mesh.vertices_where({'vertex_degree': 2}))
 mesh.vertices_attribute('is_anchor', True, keys=corners)
 
 for vertex in corners:
-    mesh.vertex_attribute(vertex, 'constraint', constraints[0])
-
-# for constraint in constraints:
-#     s = compas.json_dumps(constraint)
-#     print(s)
-#     print(constraint)
-#     print(constraint.data)
-#     print(compas.json_loads(s))
-#     # print(LineConstraint.from_data(constraint.data))
+    mesh.vertex_attribute(vertex, 'constraint', constraints[2])
 
 mesh = fofin.mesh_fd_numpy(mesh)
 
 print(mesh)
+
+for vertex in corners:
+    print(mesh.vertex_attribute(vertex, 'constraint'))
