c1c5677935bb4a6fc75cad8831ce3e27949e8133
Sam <Sam_Bouten@hotmail.com>
2021-11-11 17:57:54 +0100

    Add vector constraint

--- src/compas_fd/constraints/__init__.py
+++ src/compas_fd/constraints/__init__.py
@@ -2,17 +2,20 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+from compas.geometry import Vector
 from compas.geometry import Line
 from compas.geometry import Plane
 from compas.geometry import NurbsCurve
 from compas.geometry import NurbsSurface
 
 from .constraint import Constraint
+from .vectorconstraint import VectorConstraint
 from .lineconstraint import LineConstraint
 from .planeconstraint import PlaneConstraint
 from .curveconstraint import CurveConstraint
 from .surfaceconstraint import SurfaceConstraint
 
+Constraint.register(Vector, VectorConstraint)
 Constraint.register(Line, LineConstraint)
 Constraint.register(Plane, PlaneConstraint)
 
