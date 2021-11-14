9defa6569b958c33e8b54cc0594af2b47e9a6aff
Sam <Sam_Bouten@hotmail.com>
2021-11-14 18:01:53 +0100

    Naming

--- tests/test_hypar.py
+++ tests/test_hypar.py
@@ -10,6 +10,6 @@ def test_hypar():
     d = [0, 1, 0]
     vertices = [o, a, b, c, d]
     edges = [(0, 1), (0, 2), (0, 3), (0, 4)]
-    forcedensities = [1, 1, 1, 1]
-    result = fd_numpy(vertices=vertices, fixed=[1, 2, 3, 4], edges=edges, forcedensities=forcedensities)
+    force_densities = [1, 1, 1, 1]
+    result = fd_numpy(vertices=vertices, fixed=[1, 2, 3, 4], edges=edges, force_densities=force_densities)
     assert np.allclose(result.vertices[0], [0.5, 0.5, 0.5])
