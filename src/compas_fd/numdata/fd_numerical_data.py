a11c6f39730016eaa4f789fe0af525951741d7b7
Sam <Sam_Bouten@hotmail.com>
2021-11-15 14:35:21 +0100

    Refactor FDNumericalData
    Change FDNumericalData from dataclass into regular class with lazy calculation of property variables.
    Implicit recalculation of  dependent variables.
    Add shorthand aliases to numerical arrays.

--- src/compas_fd/numdata/fd_numerical_data.py
+++ src/compas_fd/numdata/fd_numerical_data.py
@@ -9,12 +9,12 @@ from nptyping import NDArray
 
 from numpy import asarray
 from numpy import float64
-from numpy import normrow
 from numpy import zeros_like
 from scipy.sparse import diags
 
 from compas.datastructures import Mesh
 from compas.numerical import connectivity_matrix
+from compas.numerical import normrow
 
 from compas_fd.result import Result
 from .numerical_data import NumericalData
@@ -23,44 +23,19 @@ from .face_data import FaceDataMixin
 
 class FDNumericalData(FaceDataMixin, NumericalData):
     """Stores numerical data used by the force density algorithms."""
-    free: int
-    fixed: int
-    xyz: NDArray[(Any, 3), float64]
-    C: NDArray[(Any, Any), int]
-    q: NDArray[(Any, 1), float64]
-    Q: NDArray[(Any, Any), float64]
-    p: NDArray[(Any, 1), float64]
-    A: NDArray[(Any, Any), float64]
-    Ai: NDArray[(Any, Any), float64]
-    Af: NDArray[(Any, Any), float64]
-    forces: NDArray[(Any, 1), float64] = None
-    lengths: NDArray[(Any, 1), float64] = None
-    residuals: NDArray[(Any, 3), float64] = None
-    tangent_residuals: NDArray[(Any, 3), float64] = None
-    normal_residuals: NDArray[(Any, 1), float64] = None
 
-    @classmethod
-    def from_params(cls,
-                    vertices: Union[Sequence[Annotated[List[float], 3]], NDArray[(Any, 3), float64]],
-                    fixed: List[int],
-                    edges: List[Tuple[int, int]],
-                    forcedensities: List[float],
-                    loads: Optional[Union[Sequence[Annotated[List[float], 3]], NDArray[(Any, 3), float64]]] = None
-                    ):
-        """Construct numerical arrays from force density solver input parameters."""
-        free = list(set(range(len(vertices))) - set(fixed))
-        xyz = asarray(vertices, dtype=float64).reshape((-1, 3))
-        C = connectivity_matrix(edges, 'csr')
-        Ci = C[:, free]
-        Cf = C[:, fixed]
-        q = asarray(forcedensities, dtype=float64).reshape((-1, 1))
-        Q = diags([q.flatten()], [0])
-        p = (zeros_like(xyz) if loads is None else
-             asarray(loads, dtype=float64).reshape((-1, 3)))
-        A = C.T.dot(Q).dot(C)
-        Ai = Ci.T.dot(Q).dot(Ci)
-        Af = Ci.T.dot(Q).dot(Cf)
-        return cls(free, fixed, xyz, C, q, Q, p, A, Ai, Af)
+    def __init__(self,
+                 vertices: Union[Sequence[Annotated[List[float], 3]], NDArray[(Any, 3), float64]],
+                 fixed: List[int],
+                 edges: List[Tuple[int, int]],
+                 force_densities: List[float],
+                 loads: Optional[Union[Sequence[Annotated[List[float], 3]], NDArray[(Any, 3), float64]]] = None
+                 ):
+        self.xyz = vertices
+        self.fixed = fixed
+        self.edges = edges
+        self.force_densities = force_densities
+        self.loads = loads
 
     @classmethod
     def from_mesh(cls, mesh: Mesh):
