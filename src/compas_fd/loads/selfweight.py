f1011b74ac1a2ffc7a321089dfee53ccd8f5814b
tomvanmele <tom.v.mele@gmail.com>
2024-02-06 16:55:49 +0100

    docs

--- src/compas_fd/loads/selfweight.py
+++ src/compas_fd/loads/selfweight.py
@@ -1,27 +1,78 @@
-from numpy import array
-from numpy import zeros
+from compas_fd.types import FloatNx3, FloatNx1
+
+import numpy
+import scipy.sparse
 
 from compas.geometry import cross_vectors
 from compas.geometry import length_vector
 from compas.topology import face_matrix
+from compas.datastructures import Mesh
 
 
 class SelfweightCalculator:
-    def __init__(self, mesh, density=1.0, thickness_attr_name="t"):
+    """Class for computing the selfweight of a mesh representing a surface structure
+    with a specific density and thickness.
+
+    After construction, the calculator provides a callable that can be used
+    to compute the selfweight for the current mesh coordinates.
+
+    Parameters
+    ----------
+    mesh : :class:`compas.datastructures.Mesh`
+        The mesh representing a surface structure.
+    density : float, optional
+        The density of the surface material.
+    thickness_attr_name : str, optional
+        The name of the vertex attribute storing the surface thickness.
+
+    Examples
+    --------
+    >>> from compas.datastructures import Mesh
+    >>> from compas_fd.loads import SelfWeightCalculator
+    >>> mesh = Mesh.from_meshgrid(dx=10, nx=10)
+    >>> mesh.update_default_vertex_attributes(t=0.10)
+    >>> density = 22
+    >>> calculator = SelfweightCalculator(mesh, density=density)
+    >>> xyz = mesh.vertices_attributes('xyz')
+    >>> selfweight = calculator(xyz)
+    >>> len(selfweight) == mesh.number_of_vertices()
+    True
+    >>> selfweight[0] == 0.25 * density
+    True
+
+    """
+
+    def __init__(
+        self,
+        mesh: Mesh,
+        density: float = 1.0,
+        thickness_attr_name: str = "t",
+    ):
         self.mesh = mesh
-        self.rho = array(
+        self.rho = numpy.array(
             [t * density for t in mesh.vertices_attribute(thickness_attr_name)]
         ).reshape((-1, 1))
         self.vertex_index = mesh.vertex_index()
         self.fvertex_index = {fkey: index for index, fkey in enumerate(mesh.faces())}
         self.is_loaded = {fkey: True for fkey in mesh.faces()}
-        self.F = self.face_matrix()
+        self.F = self.compute_face_matrix()
 
-    def __call__(self, xyz):
-        ta = self._tributary_areas(xyz)
+    def __call__(self, xyz: FloatNx3) -> FloatNx1:
+        ta = self.compute_tributary_areas(numpy.asarray(xyz))
         return ta * self.rho
 
-    def face_matrix(self):
+    def compute_face_matrix(self) -> scipy.sparse.csr_matrix:
+        """Compute the face matrix of the mesh.
+
+        Returns
+        -------
+        :class:`scipy.sparse.csr_matrix`
+            Number of rows is equal to the number of faces.
+            Number of columns is equal to the number of vertices.
+            Each row representa a face.
+            The row contains ones in every column corresponding to a vertex of the face.
+
+        """
         face_vertices = [None] * self.mesh.number_of_faces()
         for fkey in self.mesh.faces():
             face_vertices[self.fvertex_index[fkey]] = [
@@ -29,7 +80,20 @@ class SelfweightCalculator:
             ]
         return face_matrix(face_vertices, rtype="csr", normalize=True)
 
-    def _tributary_areas(self, xyz):
+    def compute_tributary_areas(self, xyz: FloatNx3) -> FloatNx1:
+        """Compute the tributary are of every vertex for the current coordinates.
+
+        Parameters
+        ----------
+        xyz : FloatNx3
+            The vertex coordinates.
+
+        Returns
+        -------
+        FloatNx1
+            The tributary are per vertex.
+
+        """
         mesh = self.mesh
         vertex_index = self.vertex_index
         fvertex_index = self.fvertex_index
@@ -37,7 +101,8 @@ class SelfweightCalculator:
 
         C = self.F.dot(xyz)
 
-        areas = zeros((xyz.shape[0], 1))
+        areas = numpy.zeros((xyz.shape[0], 1))
+
         for u in mesh.vertices():
             p0 = xyz[vertex_index[u]]
 
