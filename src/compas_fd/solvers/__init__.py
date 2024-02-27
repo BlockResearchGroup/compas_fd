38b7846c6e4f95300a40aaca97f732d51b68f6a4
tomvanmele <tom.v.mele@gmail.com>
2024-02-27 22:52:49 +0100

    remove mesh specific

--- src/compas_fd/solvers/__init__.py
+++ src/compas_fd/solvers/__init__.py
@@ -1,7 +1,8 @@
 from .fd_numpy import fd_numpy
 from .fd_constrained_numpy import fd_constrained_numpy
-from .mesh_fd_numpy import mesh_fd_numpy
-from .mesh_fd_constrained_numpy import mesh_fd_constrained_numpy
+
+# from .mesh_fd_numpy import mesh_fd_numpy
+# from .mesh_fd_constrained_numpy import mesh_fd_constrained_numpy
 
 # from .mesh_fd_constrained_cache import mesh_fd_constrained_cache_call  # noqa: F401
 # from .mesh_fd_constrained_cache import mesh_fd_constrained_cache_create  # noqa: F401
@@ -10,6 +11,6 @@ from .mesh_fd_constrained_numpy import mesh_fd_constrained_numpy
 __all__ = [
     "fd_numpy",
     "fd_constrained_numpy",
-    "mesh_fd_numpy",
-    "mesh_fd_constrained_numpy",
+    # "mesh_fd_numpy",
+    # "mesh_fd_constrained_numpy",
 ]
