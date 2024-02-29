74f302a115cc0be1fcfc2276417576ff1b501823
tomvanmele <tom.v.mele@gmail.com>
2024-02-29 22:17:54 +0100

    local update of tasks

--- tasks.py
+++ tasks.py
@@ -8,6 +8,7 @@ from compas_invocations import build
 from compas_invocations import docs as docs_
 from compas_invocations import tests
 from compas_invocations.console import chdir
+from compas_invocations.console import confirm
 
 
 @invoke.task(
@@ -115,6 +116,99 @@ def linkcheck(ctx, rebuild=False):
     print("Running link check...")
     opts = "-E" if rebuild else ""
     ctx.run("sphinx-build {} -b linkcheck docs dist/docs".format(opts))
+
+
+@invoke.task(
+    help={
+        "docs": "True to clean up generated documentation, otherwise False",
+        "bytecode": "True to clean up compiled python files, otherwise False.",
+        "builds": "True to clean up build/packaging artifacts, otherwise False.",
+    }
+)
+def clean(ctx, docs=True, bytecode=True, builds=True, ghuser=True):
+    """Cleans the local copy from compiled artifacts."""
+
+    with chdir(ctx.base_folder):
+        if builds:
+            ctx.run("python setup.py clean")
+
+        if bytecode:
+            for root, dirs, files in os.walk(ctx.base_folder):
+                for f in files:
+                    if f.endswith(".pyc"):
+                        os.remove(os.path.join(root, f))
+                if ".git" in dirs:
+                    dirs.remove(".git")
+
+        folders = []
+
+        if docs:
+            folders.append("docs/api/generated")
+
+        folders.append("dist/")
+
+        if bytecode:
+            for t in ("src", "tests"):
+                folders.extend(glob.glob("{}/**/__pycache__".format(t), recursive=True))
+
+        if builds:
+            folders.append("build/")
+            folders.extend(glob.glob("src/**/*.egg-info", recursive=False))
+
+        if ghuser and ctx.get("ghuser"):
+            folders.append(os.path.abspath(ctx.ghuser.target_dir))
+
+        for folder in folders:
+            shutil.rmtree(os.path.join(ctx.base_folder, folder), ignore_errors=True)
+
+
+@invoke.task(help={"release_type": "Type of release follows semver rules. Must be one of: major, minor, patch."})
+def release(ctx, release_type):
+    """Releases the project in one swift command!"""
+    if release_type not in ("patch", "minor", "major"):
+        raise invoke.Exit("The release type parameter is invalid.\nMust be one of: major, minor, patch.")
+
+    # Run formatter
+    ctx.run("invoke format")
+
+    # Run checks
+    ctx.run("invoke check test")
+
+    # Bump version and git tag it
+    ctx.run("bump2version %s --verbose" % release_type)
+
+    # Build project
+    ctx.run("python setup.py clean --all sdist bdist_wheel")
+
+    # Prepare the change log for the next release
+    prepare_changelog(ctx)
+
+    # Clean up local artifacts
+    clean(ctx)
+
+    # Upload to pypi
+    if confirm(
+        "Everything is ready. You are about to push to git which will trigger a release to pypi.org. Are you sure?",
+        assume_yes=False,
+    ):
+        ctx.run("git push --tags && git push")
+    else:
+        raise invoke.Exit("You need to manually revert the tag/commits created.")
+
+
+@invoke.task
+def prepare_changelog(ctx):
+    """Prepare changelog for next release."""
+    UNRELEASED_CHANGELOG_TEMPLATE = "## Unreleased\n\n### Added\n\n### Changed\n\n### Removed\n\n\n## "
+
+    with chdir(ctx.base_folder):
+        # Preparing changelog for next release
+        with open("CHANGELOG.md", "r+") as changelog:
+            content = changelog.read()
+            changelog.seek(0)
+            changelog.write(content.replace("## ", UNRELEASED_CHANGELOG_TEMPLATE, 1))
+
+        ctx.run('git add CHANGELOG.md && git commit -m "Prepare changelog for next release"')
 
 
 ns = Collection(
